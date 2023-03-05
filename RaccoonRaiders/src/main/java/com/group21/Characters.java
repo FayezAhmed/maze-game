b939f24718be9335b23601307c613cc7482d03e1
Junui <jha357@sfu.ca>
2023-03-05 15:52:35 -0800

    setter, getter, isDead, move methods added

--- RaccoonRaiders/src/main/java/com/group21/Characters.java
+++ RaccoonRaiders/src/main/java/com/group21/Characters.java
@@ -4,7 +4,7 @@ public abstract class Characters {
     // Characters' common instance variables
     private double health;
     private double damage;
-    private Position position;
+    private Position position = null;
     private int speed;
 
     Characters(){
@@ -13,4 +13,49 @@ public abstract class Characters {
         position = new Position();
         speed = 5;
     }
+    
+    public double getHealth(){
+        return health;
+    }
+
+    public double getDamage(){
+        return damage;
+    }
+
+    public Position getPosition(){
+        return position;
+    }
+
+    public int getSpeed(){
+        return speed;
+    }
+
+    public void setHealth(double newHealth){
+        health = newHealth;
+    }
+
+    public void setDamage(double newDamange){
+        damage = newDamange;
+    }
+
+    public void setPosition(int x, int y){
+        position.setX_position(x);
+        position.setY_position(y);
+    }
+
+    public void setPosition(Position newPosition){
+        position = newPosition;
+    }
+
+    public void setSpeed(int newSpeed){
+        speed = newSpeed;
+    }
+
+    public boolean isDead(){
+        if (health <= 0){
+            return true;
+        }
+        return false;
+    }
+    public abstract void move(Direction dir);
 }
