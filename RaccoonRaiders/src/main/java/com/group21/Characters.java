1d32f48dcb0c6f4dbd730d4d74bb15d0e746a2bc
Junui <jha357@sfu.ca>
2023-03-05 14:15:30 -0800

    Character class instance variables are added

--- RaccoonRaiders/src/main/java/com/group21/Characters.java
+++ RaccoonRaiders/src/main/java/com/group21/Characters.java
@@ -1,7 +1,9 @@
 package com.group21;
 
 public abstract class Characters {
-    
-    
-    
+    // Characters' common instance variables
+    private double health;
+    private double damage;
+    private Position position;
+    private int speed;
 }
