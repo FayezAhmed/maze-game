14d1c4494339873d3b1f72e1c6c72d99071b9e54
Fayez Ahmed <fayezkaleemahmed@gmail.com>
2023-03-07 21:04:41 -0800

    Fixed error regarding abstract method.

--- RaccoonRaiders/src/main/java/com/group21/Enemy.java
+++ RaccoonRaiders/src/main/java/com/group21/Enemy.java
@@ -1,5 +1,11 @@
 package com.group21;
 
 public class Enemy extends Characters{
+
+    @Override
+    public void move(Direction dir) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'move'");
+    }
     
 }
--- RaccoonRaiders/src/main/java/com/group21/Student.java
+++ RaccoonRaiders/src/main/java/com/group21/Student.java
@@ -1,5 +1,11 @@
 package com.group21;
 
 public class Student extends Characters{
+
+    @Override
+    public void move(Direction dir) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'move'");
+    }
     
 }
