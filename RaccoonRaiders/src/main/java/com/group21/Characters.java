a101bdb496da4b618599700d74a73506407e737f
Jun (Junui) Hong <jhong980821@outlook.com>
2023-03-10 01:15:52 -0800

    Characters (abstract) class added with character default variables and abstract methods

---
+++ RaccoonRaiders/src/main/java/com/group21/Characters.java
@@ -0,0 +1,25 @@
+package com.group21;
+
+import java.awt.image.BufferedImage;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
+public abstract class Characters {
+    
+    // Character Variables
+    public int x, y;    
+    public int speed;
+    public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+    public String direction;
+    public boolean collisionOn = false;
+    public Rectangle solidArea;
+    public int score = 0;
+
+    public int solidAreaDefaultX, solidAreaDefaultY;
+
+    public int spriteCounter = 0;
+    public int spriteNumber = 1;
+
+    public abstract void update();
+    public abstract void draw(Graphics2D g2);
+}
