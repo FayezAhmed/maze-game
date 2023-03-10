7225de3c26936ba8594efaa88bb26ef4026e5783
Jun (Junui) Hong <jhong980821@outlook.com>
2023-03-10 02:01:47 -0800

    comments added

--- RaccoonRaiders/src/main/java/com/group21/Coffee.java
+++ RaccoonRaiders/src/main/java/com/group21/Coffee.java
@@ -4,10 +4,15 @@ import java.io.IOException;
 import java.awt.Graphics2D;
 import javax.imageio.ImageIO;
 
+/**
+ * Class Coffee -- rewarding scores
+ */
 public class Coffee extends Items{
     
+    /**
+     * Constructor
+     */
     public Coffee(){
-
         name = "Coffee";
         try {
             image = ImageIO.read(getClass().getResourceAsStream("/items_image/coffee.png"));
@@ -17,6 +22,11 @@ public class Coffee extends Items{
         collision = true;
     }
 
+    /**
+     * draw onto panel
+     * @param g2 Graphic
+     * @param GamePanel target panel to draw on
+     */
     public void draw(Graphics2D g2, GamePanel gp){
         g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
     }
