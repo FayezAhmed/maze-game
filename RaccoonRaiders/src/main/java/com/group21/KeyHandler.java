602a18db98fde419fd23e5d4ff29f379d1b5a72d
Jun (Junui) Hong <jhong980821@outlook.com>
2023-03-10 01:24:04 -0800

    KeyHandler class added with methods that are allowing interaction between keyboard inputs

---
+++ RaccoonRaiders/src/main/java/com/group21/KeyHandler.java
@@ -0,0 +1,56 @@
+package com.group21;
+import java.awt.event.*;
+/**
+ * KeyHanler class -- interact with Students <--> Keyboard input from player
+ */
+public class KeyHandler implements KeyListener {
+
+    public boolean up, down, right, left;
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        
+        int code = e.getKeyCode();
+
+        switch(code){
+            case KeyEvent.VK_UP:
+                up = true;
+                break;
+            case KeyEvent.VK_DOWN:
+                down = true;
+                break;
+            case KeyEvent.VK_RIGHT:
+                right = true;
+                break;
+            case KeyEvent.VK_LEFT:
+                left = true;
+                break;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int code = e.getKeyCode();
+
+        switch(code){
+            case KeyEvent.VK_UP:
+                up = false;
+                break;
+            case KeyEvent.VK_DOWN:
+                down = false;
+                break;
+            case KeyEvent.VK_RIGHT:
+                right = false;
+                break;
+            case KeyEvent.VK_LEFT:
+                left = false;
+                break;
+        }
+    }
+    
+}
