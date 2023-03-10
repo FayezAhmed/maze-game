928990b500bf3c55a69a8d336d3790b5df9ddf7d
Jun (Junui) Hong <jhong980821@outlook.com>
2023-03-10 02:35:22 -0800

    CollisionChecker, items, AssetSetter setup game, painComponent updated

--- RaccoonRaiders/src/main/java/com/group21/GamePanel.java
+++ RaccoonRaiders/src/main/java/com/group21/GamePanel.java
@@ -30,16 +30,26 @@ public class GamePanel extends JPanel implements Runnable{
     KeyHandler key = new KeyHandler();
     Student student = new Student(this, key);
     TileManager tm = new TileManager(this);
+    CollisionChecker cChecker = new CollisionChecker(this);
+    Items items[] = new Items[10];
+    AssetSetter setter = new AssetSetter(this);
 
     /**
      * Default Constructor. Creates Game Panel 
      */
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth, screenHeight)); // setup size
-        this.setBackground(Color.BLACK);
+        this.setBackground(Color.black);
         this.setDoubleBuffered(true);
         this.addKeyListener(key);
         this.setFocusable(true); // let GamePanel focus to recieve key input
+    }
+
+    /**
+     * setting up the items on panel
+     */
+    public void setupGame(){
+        setter.setObject();
     }
 
     /**
@@ -85,15 +95,19 @@ public class GamePanel extends JPanel implements Runnable{
      * draw the components on the panel
      * @param g Graphic 
      */
-    public void printComponent(Graphics g){
+    public void paintComponent(Graphics g){
         super.paintComponent(g);
 
         Graphics2D g2 = (Graphics2D) g;
 
         tm.draw(g2);
 
+        for (int i = 0; i < items.length; i++){
+            if (items[i] != null)
+                items[i].draw(g2, this);
+        }
         student.draw(g2);
 
-        g2.dispose();   // dispose of this graphics contxt and release any system resources that it is using
+        g2.dispose();   // dispose of this graphics contxt and release any system resources that it is using    }
     }
 }
