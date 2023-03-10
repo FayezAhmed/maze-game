5f69a331adf57e361461a801b41ff99aeedf3270
Jun (Junui) Hong <jhong980821@outlook.com>
2023-03-10 01:06:56 -0800

    game setting for GamePanel and JFrame added

---
+++ RaccoonRaiders/src/main/java/com/group21/Launcher.java
@@ -0,0 +1,20 @@
+package com.group21;
+
+import javax.swing.JFrame;
+
+public class Launcher {
+    public static void main(String[] args){
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("RaccooN RaiderS");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
