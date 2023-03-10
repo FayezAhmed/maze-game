e23505331e5770c34b4a2a406f63765952438a72
Jun (Junui) Hong <jhong980821@outlook.com>
2023-03-10 01:30:09 -0800

    Tile class added with default variables

---
+++ RaccoonRaiders/src/main/java/com/group21/Tile.java
@@ -0,0 +1,13 @@
+package com.group21;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * Tile Class
+ */
+public class Tile {
+
+    // Default Variables
+    public BufferedImage image;
+    public boolean collision = false;   
+}
