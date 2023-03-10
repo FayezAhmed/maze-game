e40be68d89ceb695bb601d3e622a96a7c9d686f5
Jun (Junui) Hong <jhong980821@outlook.com>
2023-03-10 01:21:10 -0800

    Items (abstract) class added with default variables and abstact method

---
+++ RaccoonRaiders/src/main/java/com/group21/Items.java
@@ -0,0 +1,22 @@
+package com.group21;
+
+import java.awt.image.BufferedImage;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
+/**
+ * Abstract Items Class
+ */
+public abstract class Items {
+
+    //default variables
+    public BufferedImage image;
+    public String name;
+    public boolean collision = false;
+    public int x,y;
+    public Rectangle solidArea = new Rectangle(0,0,48,48);
+    public int solidAreaDefaultX = 0;
+    public int solidAreaDefaultY = 0;
+
+    public abstract void draw(Graphics2D g2, GamePanel gp);
+}
