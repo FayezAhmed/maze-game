6000055f625c19518828fd4207dce28e15ef3503
Junui <jha357@sfu.ca>
2023-03-10 15:28:51 -0800

    Score has been added

--- RaccoonRaiders/src/main/java/com/group21/Coffee.java
+++ RaccoonRaiders/src/main/java/com/group21/Coffee.java
@@ -14,6 +14,7 @@ public class Coffee extends Items{
      */
     public Coffee(){
         name = "Coffee";
+        score = 50;
         try {
             image = ImageIO.read(getClass().getResourceAsStream("/items_image/coffee.png"));
         } catch(IOException e){
