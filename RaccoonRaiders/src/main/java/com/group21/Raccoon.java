97a8537aeb0b666b0d3b7dada8e6c8058a62d344
Fayez Ahmed <fayezkaleemahmed@gmail.com>
2023-03-11 11:42:30 -0800

    Get raccoon images.

--- RaccoonRaiders/src/main/java/com/group21/Raccoon.java
+++ RaccoonRaiders/src/main/java/com/group21/Raccoon.java
@@ -1,6 +1,9 @@
 package com.group21;
 
 import java.awt.Graphics2D;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Raccoon extends Characters{
     
@@ -9,6 +12,8 @@ public class Raccoon extends Characters{
 
         direction = "down";
         speed = 5;
+
+        getImage();
 
     }
 
@@ -23,4 +28,24 @@ public class Raccoon extends Characters{
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'draw'");
     }
+
+    /**
+     * Read the student sprite images 
+     */
+    public void getImage(){
+        try{
+            up1 = ImageIO.read(getClass().getResourceAsStream("/raccoon_image/raccoon_up_1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/raccoon_image/raccoon_up_2.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/raccoon_image/raccoon_down_1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/raccoon_image/raccoon_down_2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/raccoon_image/raccoon_left_1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/raccoon_image/raccoon_left_2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/raccoon_image/raccoon_right_1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/raccoon_image/raccoon_right_2.png"));
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
 }
