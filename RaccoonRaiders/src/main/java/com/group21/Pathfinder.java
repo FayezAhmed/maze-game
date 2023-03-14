c5090c2ef66d5ba6a72ca22d1cdfdb4b121d69aa
Fayez Ahmed <fayezkaleemahmed@gmail.com>
2023-03-14 01:33:18 -0700

    Set up variables of pathfinding class.

--- RaccoonRaiders/src/main/java/com/group21/Pathfinder.java
+++ RaccoonRaiders/src/main/java/com/group21/Pathfinder.java
@@ -1,5 +1,14 @@
 package com.group21;
 
+import java.util.ArrayList;
+
 public class Pathfinder {
     
+    GamePanel gp;
+    Node[][] node;
+    ArrayList<Node> openList = new ArrayList<>();
+    public ArrayList<Node> pathList = new ArrayList<>();
+    Node startNode, goalNode, currentNode;
+    boolean goalReached = false;
+    int step = 0;
 }
