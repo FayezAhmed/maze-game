15657c24c6fbee91c9a7b2f8c919d561309627ea
Jun (Junui) Hong <jhong980821@outlook.com>
2023-03-13 20:00:58 -0700

    adding more rewards

--- RaccoonRaiders/src/main/java/com/group21/AssetSetter.java
+++ RaccoonRaiders/src/main/java/com/group21/AssetSetter.java
@@ -20,32 +20,60 @@ public class AssetSetter {
      */
     public void setObject(){
         gp.rewards[0] = new Coffee();
-        gp.rewards[0].x = 20 * gp.tileSize;
-        gp.rewards[0].y = 20 * gp.tileSize;
+        gp.rewards[0].x = 1 * gp.tileSize;
+        gp.rewards[0].y = 22 * gp.tileSize;
 
         gp.rewards[1] = new Coffee();
-        gp.rewards[1].x = 30 * gp.tileSize;
-        gp.rewards[1].y = 10 * gp.tileSize;
+        gp.rewards[1].x = 1 * gp.tileSize;
+        gp.rewards[1].y = 7 * gp.tileSize;
 
         gp.rewards[2] = new BubbleTea();
-        gp.rewards[2].x = 13 * gp.tileSize;
-        gp.rewards[2].y = 4 * gp.tileSize;
+        gp.rewards[2].x = 23 * gp.tileSize;
+        gp.rewards[2].y = 3 * gp.tileSize;
 
-        gp.punishments[3] = new Punishment();
-        gp.punishments[3].x = 21 * gp.tileSize;
-        gp.punishments[3].y = 9 * gp.tileSize;
+        gp.rewards[3] = new Coffee();
+        gp.rewards[3].x = 11 * gp.tileSize;
+        gp.rewards[3].y = 1 * gp.tileSize;
 
-        gp.punishments[4] = new Punishment();
-        gp.punishments[4].x = 22 * gp.tileSize;
-        gp.punishments[4].y = 3 * gp.tileSize;
+        gp.rewards[4] = new Coffee();
+        gp.rewards[4].x = 8 * gp.tileSize;
+        gp.rewards[4].y = 13 * gp.tileSize;
 
-        gp.punishments[5] = new Punishment();
-        gp.punishments[5].x = 24 * gp.tileSize;
-        gp.punishments[5].y = 5 * gp.tileSize;
+        gp.rewards[5] = new BubbleTea();
+        gp.rewards[5].x = 6 * gp.tileSize;
+        gp.rewards[5].y = 16 * gp.tileSize;
 
-        gp.punishments[6] = new Punishment();
-        gp.punishments[6].x = 25 * gp.tileSize;
-        gp.punishments[6].y = 8 * gp.tileSize;
+        gp.rewards[6] = new BubbleTea();
+        gp.rewards[6].x = 36 * gp.tileSize;
+        gp.rewards[6].y = 12 * gp.tileSize;
+
+        gp.rewards[7] = new Coffee();
+        gp.rewards[7].x = 30 * gp.tileSize;
+        gp.rewards[7].y = 12 * gp.tileSize;
+
+        gp.rewards[8] = new Coffee();
+        gp.rewards[8].x = 24 * gp.tileSize;
+        gp.rewards[8].y = 11 * gp.tileSize;
+
+        gp.rewards[9] = new BubbleTea();
+        gp.rewards[9].x = 36 * gp.tileSize;
+        gp.rewards[9].y = 12 * gp.tileSize;
+
+        // gp.punishments[3] = new Punishment();
+        // gp.punishments[3].x = 21 * gp.tileSize;
+        // gp.punishments[3].y = 9 * gp.tileSize;
+
+        // gp.punishments[4] = new Punishment();
+        // gp.punishments[4].x = 22 * gp.tileSize;
+        // gp.punishments[4].y = 3 * gp.tileSize;
+
+        // gp.punishments[5] = new Punishment();
+        // gp.punishments[5].x = 24 * gp.tileSize;
+        // gp.punishments[5].y = 5 * gp.tileSize;
+
+        // gp.punishments[6] = new Punishment();
+        // gp.punishments[6].x = 25 * gp.tileSize;
+        // gp.punishments[6].y = 8 * gp.tileSize;
     }
 
     public void setRaccoon() {
