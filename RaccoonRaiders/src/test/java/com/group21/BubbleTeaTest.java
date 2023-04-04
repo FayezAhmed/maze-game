6c0d196174b2625d4011fe63344c520a2321d8fb
Jun (Junui) Hong <jhong980821@outlook.com>
2023-04-04 16:30:03 -0700

    BubbleTeaTest added

---
+++ RaccoonRaiders/src/test/java/com/group21/BubbleTeaTest.java
@@ -0,0 +1,36 @@
+package com.group21;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BubbleTeaTest {
+
+    private Items bubbletea;
+
+    @BeforeEach
+    public void setup(){
+        bubbletea = new BubbleTea();
+    }
+
+    @Test
+    public void imageTest(){
+        assertNotNull(bubbletea.image);
+    }
+
+    @Test
+    public void nameTest(){
+        assertEquals(bubbletea.name, "BubbleTea");
+    }
+
+    @Test
+    public void scoreTest(){
+        assertEquals(100, bubbletea.score);
+    }
+
+    @Test
+    public void collisionTest(){
+        assertTrue(bubbletea.collision);
+    }
+}
