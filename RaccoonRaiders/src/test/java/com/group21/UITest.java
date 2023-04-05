9d58a68170c8e39d08984e6bdc91d4c11641d10b
qijunz <qijunz@sfu.ca>
2023-04-05 09:12:13 +0800

    change in UI and sound test

--- RaccoonRaiders/src/test/java/com/group21/UITest.java
+++ RaccoonRaiders/src/test/java/com/group21/UITest.java
@@ -2,47 +2,32 @@ package com.group21;
 
 import static org.junit.Assert.*;
 
-import java.awt.Font;
-import java.awt.Graphics2D;
-
 import org.junit.Before;
 import org.junit.Test;
 
 public class UITest {
 
     private UI UI;
+    private GamePanel gp;
 
     @Before
     public void setUp() {
-        GamePanel gp = new GamePanel();
+        gp = new GamePanel();
         UI = new UI(gp);
     }
-
     @Test
-    public void testDrawTitle() {
-        Graphics2D g2 = null; // You can mock the Graphics2D object if necessary
-        UI.drawTitle();
-        assertEquals(UI.g2.getFont(), UI.retroFont);
-        assertEquals(UI.g2.getFont().getStyle(), Font.PLAIN);
-        assertEquals(UI.g2.getFont().getSize(), 100f, 0.1f);
-        assertEquals(UI.getCenterX("RACCOON RAIDERS"), 341);
+    public void fileTest(){
+        assertNotNull(UI.heartImage);
+        assertNotNull(UI.titleImage);
+        assertNotNull(UI.raccoonImage);
     }
 
     @Test
-    public void testDrawPause() {
-        Graphics2D g2 = null; // You can mock the Graphics2D object if necessary
-        UI.drawPause();
+    public void DrawTest() {
+        UI.draw();//call draw method
         assertEquals(UI.g2.getFont(), UI.retroFont);
         assertEquals(UI.g2.getFont().getStyle(), Font.PLAIN);
-        assertEquals(UI.g2.getFont().getSize(), 150f, 0.1f);
-        assertEquals(UI.getCenterX("PAUSED"), 395);
-    }
-
-    @Test
-    public void testDraw() {
-        Graphics2D g2 = null; // You can mock the Graphics2D object if necessary
-        UI.draw(g2);
-        // Add more assertions if necessary
+        assertEquals(UI.g2.getColor(), Color.gray);
     }
 
 }
