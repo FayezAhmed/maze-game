7c73725d7e4de6e57cd35bfdc34a5e4749ed9a4d
Fayez Ahmed <fayezkaleemahmed@gmail.com>
2023-04-12 16:45:58 -0700

    Updated the javadocs comments.

--- RaccoonRaiders/src/main/java/com/group21/Node.java
+++ RaccoonRaiders/src/main/java/com/group21/Node.java
@@ -1,18 +1,53 @@
 package com.group21;
 
 /**
- * Node class used for pathfinding algorithm
+ * Node class used for pathfinding algorithm.
  */
 public class Node {
     
+    /**
+     * Parent of the node.
+     */
     Node parent;
+
+    /**
+     * The column position of the node.
+     */
     public int col;
+
+    /**
+     * The row position of the node.
+     */
     public int row;
+
+    /**
+     * The cost of moving from the start node to this node.
+     */
     int gCost;
+
+    /**
+     * The estimated cost of moving from this node to the end node.
+     */
     int hCost;
+
+    /**
+     * The sum of the gCost and hCost.
+     */
     int fCost;
+
+    /**
+     * A boolean indicating whether this node is impassable.
+     */
     boolean solid;
+
+    /**
+     * A boolean indicating whether this node is open.
+     */
     boolean open;
+
+    /**
+     * A boolean indicating whether this node has been checked.
+     */
     boolean checked;
 
     /**
